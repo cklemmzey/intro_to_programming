057a2b93d9eee64cb76abad6bd88fa368b3ba0f7
Clement Udensi <cklemzey@gmail.com>
2016-10-04 06:09:51 +0100

    fixed thirteen error

--- 092716/fiw.rb
+++ 092716/fiw.rb
@@ -1,31 +1,40 @@
 def convert_to_words (num)
   zero = {0 => 'zero'}
   unit = {1 => 'one', 2 => 'two', 3 => 'three', 4 => 'four', 5 => 'five', 6 => 'six', 7 => 'seven', 8 => 'eight', 9 => 'nine'}
-  tens = {1 => 'eleven', 2 => 'twelve', 3 => 'three', 4 => 'fourteen', 5 => 'fifteen', 6 => 'sixten', 7 => 'seventeen', 8 => 'eighteen', 9 => 'nineteen'}
+  tens = {1 => 'eleven', 2 => 'twelve', 3 => 'thirteen', 4 => 'fourteen', 5 => 'fifteen', 6 => 'sixten', 7 => 'seventeen', 8 => 'eighteen', 9 => 'nineteen'}
   multiples_tens = {1 => 'ten', 2 => 'twenty', 3 => 'thirty', 4 => 'forty', 5 => 'fifty', 6 => 'sixty', 7 => 'seventy', 8 => 'eighty', 9 => 'ninety'}
 
   conversion = ""
 
-words = num
-if words.is_a?(Integer)
- words
-else
-puts "this is not a valid number"
-end
+  words = num
+  if words.is_a?(Integer)
+    words
+  else
+    puts "this is not a valid number"
+  end
 
-words = num
-if words == 0
-conversion << zero[words]
-end
+  words = num
+  if words == 0
+    conversion << zero[words]
+  end
 
-words = num
-if words < 0
-words = words * -1
-return convert_to_words(words).insert(0, "negative ")
-end
+  words = num
+  if words < 0
+    words = words * -1
+    return convert_to_words(words).insert(0, "negative ")
+  end
+
+  words = num / 1000000000000
+  remainder = num % 1000000000000
+
+  if words > 0
+    conversion << convert_to_words(words) << " trillion "
+    num = remainder
+  end
+
 
 words = num / 1000000000
-  remainder = num % 1000000000
+remainder = num % 1000000000
 
   if words > 0
     conversion << convert_to_words(words) << " billion "
@@ -86,7 +95,6 @@ words = num / 1000000000
   end
 
   words = remainder
-
   
   if words > 0 
     conversion << unit[words]
